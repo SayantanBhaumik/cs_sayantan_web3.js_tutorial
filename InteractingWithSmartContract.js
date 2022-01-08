e1b28500e23fa6369182800b38ce243c6bf08ec9
cs_sayantan <84985055+SayantanBhaumik@users.noreply.github.com>
2022-01-08 14:55:26 +0530

    Create InteractingWithSmartContract.js

---
+++ InteractingWithSmartContract.js
@@ -0,0 +1,5 @@
+//smart contracts are written n solidity
+//they are compiled to bytocode to run on EVM
+//web3.eth.Contract creates an abstraction of the smart contract that is residing in the ethereum blockchain
+//we can then interact with the functions ofthe smart contract with javascript
+//smart contract ABI gives out info about smart contract in JSON format
